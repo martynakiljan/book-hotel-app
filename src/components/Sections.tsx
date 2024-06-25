a7326814cbe1452d729450842e3d919d86ee2a01
martyna.kiljan <martyna.kiljan@nsgroup.ch>
2024-06-25 21:03:00 +0200

    added basename

--- src/components/Sections.tsx
+++ src/components/Sections.tsx
@@ -1,37 +1,35 @@
-/** @format */
-
-import React from 'react'
-import { Routes, Route } from 'react-router-dom'
-import Hotels from './Hotels'
-import About from './About'
-import Home from './Home'
-import Vouchers from './Vouchers'
-import Contact from './Contact'
-import HotelDetails from './HotelDetails'
-import Imprint from './Imprint'
-import Booking from './Booking'
-import BookingConfirmation from './BookingConfirmation'
-import Finalization from './Finalization'
-import ScrollToTop from './ScrollToTop'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Hotels from "./Hotels";
+import About from "./About";
+import Home from "./Home";
+import Vouchers from "./Vouchers";
+import Contact from "./Contact";
+import HotelDetails from "./HotelDetails";
+import Imprint from "./Imprint";
+import Booking from "./Booking";
+import BookingConfirmation from "./BookingConfirmation";
+import Finalization from "./Finalization";
+import ScrollToTop from "./ScrollToTop";
 
 const Sections = () => {
-	return (
-		<>
-			<ScrollToTop />
-			<Routes>
-				<Route path='/' element={<Home />} />
-				<Route path='/hotels' element={<Hotels />} />
-				<Route path='/hotel/:id' element={<HotelDetails />} />
-				<Route path='/about' element={<About />} />
-				<Route path='/vouchers' element={<Vouchers />} />
-				<Route path='/contact' element={<Contact />} />
-				<Route path='/imprint' element={<Imprint />} />
-				<Route path='/booking' element={<Booking />} />
-				<Route path='/finalization' element={<Finalization />} />
-				<Route path='/booking-confirmation' element={<BookingConfirmation />} />
-			</Routes>
-		</>
-	)
-}
+  return (
+    <BrowserRouter basename="/book-hotel-app">
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/hotels" element={<Hotels />} />
+        <Route path="/hotel/:id" element={<HotelDetails />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/vouchers" element={<Vouchers />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/imprint" element={<Imprint />} />
+        <Route path="/booking" element={<Booking />} />
+        <Route path="/finalization" element={<Finalization />} />
+        <Route path="/booking-confirmation" element={<BookingConfirmation />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-export default Sections
+export default Sections;
